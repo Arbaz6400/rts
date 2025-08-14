3d62a76f3417d6f865503e9867bd3aab843e3b4b
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-14 13:12:49 +0530

    Update dispatcher.groovy

--- dispatcher.groovy
+++ dispatcher.groovy
@@ -1,10 +1,6 @@
 node {
-    // Parameter to choose which groovy to run
     def scriptName = params.PIPELINE_NAME ?: 'build.groovy'
 
-    echo "Selected pipeline script: ${scriptName}"
-
-    // Checkout the RTS repo in a subfolder
     dir('rts') {
         checkout([
             $class: 'GitSCM',
@@ -14,12 +10,11 @@ node {
             userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
         ])
 
-        // Full path to the selected Groovy script
         def scriptPath = "${scriptName}"
-
         if (fileExists(scriptPath)) {
             echo "Loading script from: ${scriptPath}"
-            load(scriptPath)
+            def loadedScript = load(scriptPath)
+            loadedScript.executeBuild() // Call custom entry point
         } else {
             error "Script '${scriptPath}' not found in RTS repo"
         }
