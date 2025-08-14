3944d3bf3205adc34024d3d11730bfbd5bec55f3
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-14 13:38:28 +0530

    Update dispatcher.groovy

--- dispatcher.groovy
+++ dispatcher.groovy
@@ -1,21 +1,27 @@
-node {
-    def scriptName = params.PIPELINE_NAME ?: 'build.groovy'
+// dispatcher.groovy
+def runPipeline(defaultScript) {
+    node {
+        // Use param if passed, else default from streaming repo Jenkinsfile
+        def scriptName = params.PIPELINE_NAME ?: defaultScript
 
-    dir('rts') {
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: "*/main"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [],
-            userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
-        ])
+        dir('rts') {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: "*/main"]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [],
+                userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
+            ])
 
-        if (fileExists(scriptName)) {
-            echo "Loading and executing: ${scriptName}"
-            def loadedScript = load(scriptName)
-            loadedScript.executePipeline() // our custom entry point
-        } else {
-            error "Script '${scriptName}' not found in RTS repo"
+            if (fileExists(scriptName)) {
+                echo "Loading and executing: ${scriptName}"
+                def loadedScript = load(scriptName)
+                loadedScript.executePipeline()
+            } else {
+                error "❌ Script '${scriptName}' not found in RTS repo"
+            }
         }
     }
 }
+
+return this
