cace4c88a1760bab2e78a3aa25d7274d4d1c31bf
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-14 13:10:10 +0530

    Update dispatcher.groovy

--- dispatcher.groovy
+++ dispatcher.groovy
@@ -1,21 +1,27 @@
-// Parameter should be passed from Streaming repo Jenkinsfile
-def scriptName = params.PIPELINE_NAME ?: 'build.groovy'
+node {
+    // Parameter to choose which groovy to run
+    def scriptName = params.PIPELINE_NAME ?: 'build.groovy'
 
-// Always checkout RTS repo into a separate folder so files don't get wiped
-dir('rts') {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/main"]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'rts']],
-        userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
-    ])
+    echo "Selected pipeline script: ${scriptName}"
 
-    def scriptPath = "rts/${scriptName}"
-    if (fileExists(scriptPath)) {
-        echo "Loading RTS script: ${scriptPath}"
-        load(scriptPath)
-    } else {
-        error "Script ${scriptPath} not found in RTS repo"
+    // Checkout the RTS repo in a subfolder
+    dir('rts') {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/main"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [],
+            userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
+        ])
+
+        // Full path to the selected Groovy script
+        def scriptPath = "${scriptName}"
+
+        if (fileExists(scriptPath)) {
+            echo "Loading script from: ${scriptPath}"
+            load(scriptPath)
+        } else {
+            error "❌ Script '${scriptPath}' not found in RTS repo"
+        }
     }
 }
