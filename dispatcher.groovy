c962b2500ee38d858450d905a31cbe0eff0df97d
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-14 13:09:02 +0530

    Update dispatcher.groovy

--- dispatcher.groovy
+++ dispatcher.groovy
@@ -1,28 +1,21 @@
-pipeline {
-    agent any
+// Parameter should be passed from Streaming repo Jenkinsfile
+def scriptName = params.PIPELINE_NAME ?: 'build.groovy'
 
-    stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Arbaz6400/Streaming.git'
-            }
-        }
+// Always checkout RTS repo into a separate folder so files don't get wiped
+dir('rts') {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/main"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'rts']],
+        userRemoteConfigs: [[url: 'https://github.com/Arbaz6400/rts.git']]
+    ])
 
-        stage('Load and Run Scripts') {
-            steps {
-                script {
-                    def scripts = ['build.groovy', 'test.groovy', 'deploy.groovy']
-                    for (s in scripts) {
-                        def scriptPath = "${env.WORKSPACE}/${s}"
-                        if (fileExists(scriptPath)) {
-                            echo "Loading ${s}"
-                            load scriptPath
-                        } else {
-                            error "Missing script: ${s}"
-                        }
-                    }
-                }
-            }
-        }
+    def scriptPath = "rts/${scriptName}"
+    if (fileExists(scriptPath)) {
+        echo "Loading RTS script: ${scriptPath}"
+        load(scriptPath)
+    } else {
+        error "Script ${scriptPath} not found in RTS repo"
     }
 }
