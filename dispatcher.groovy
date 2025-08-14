23115ada8fa04bc2e2de49a7d01db12efea59dba
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-14 13:03:03 +0530

    Update dispatcher.groovy

--- dispatcher.groovy
+++ dispatcher.groovy
@@ -1,19 +1,26 @@
 pipeline {
     agent any
-    parameters {
-        choice(
-            name: 'PIPELINE_NAME',
-            choices: ['build.groovy', 'deploy.groovy'],
-            description: 'Select which RTS pipeline to run'
-        )
-    }
+
     stages {
-        stage('Run Selected Pipeline') {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Arbaz6400/Streaming.git'
+            }
+        }
+
+        stage('Load and Run Scripts') {
             steps {
                 script {
-                    def scriptPath = "${env.WORKSPACE}/${params.PIPELINE_NAME}"
-                    echo "Loading RTS pipeline: ${scriptPath}"
-                    load(scriptPath)
+                    def scripts = ['build.groovy', 'test.groovy', 'deploy.groovy']
+                    for (s in scripts) {
+                        def scriptPath = "${env.WORKSPACE}/${s}"
+                        if (fileExists(scriptPath)) {
+                            echo "Loading ${s}"
+                            load scriptPath
+                        } else {
+                            error "Missing script: ${s}"
+                        }
+                    }
                 }
             }
         }
