bb95292638e2d6aeeaa73c969a4778a4d2662e31
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-15 13:16:34 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,6 +1,18 @@
 pipeline {
     agent any
     stages {
+        stage('Check for Tag') {
+            steps {
+                script {
+                    if (env.TAG_NAME && env.TAG_NAME.trim()) {
+                        error "❌ This job cannot run on a tag build! Tag detected: ${env.TAG_NAME}"
+                    } else {
+                        echo "✅ No tag detected. Proceeding..."
+                    }
+                }
+            }
+        }
+
         stage('Test Stage') {
             steps {
                 echo "✅ Test Groovy from RTS repo executed successfully!"
