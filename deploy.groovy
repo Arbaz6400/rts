1cabb85ff6dc63df0102e89b8467acb1b6718ad8
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-16 14:27:13 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,10 +1,19 @@
-def executePipeline() {
-    stage('Build') {
-        echo "deploy..."
+pipeline {
+    agent any
+    stages {
+        stage('Check for Tag') {
+            steps {
+                script {
+                    // Explicit UTF-8 reading mode
+                    
+                        echo "✅ No tag detected. deploy.groovy Proceeding..."
+                    }
+                }
+            }
     }
-    stage('Test') {
-        echo "Running tests..."
+    post {
+        always {
+            echo "✅ Finished test pipeline."
+        }
     }
 }
-
-return this
