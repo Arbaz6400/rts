4349b71d553755f4a8f2b7792367c9e1440ea6b1
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-18 12:32:45 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -2,13 +2,13 @@ def repoName = env.JOB_NAME.tokenize('/')[0]   // extract trigger repo name
 
 def scriptPath = ""
 switch (repoName) {
-    case "service-a":
+    case "Leap1":
         scriptPath = "build.groovy"
         break
-    case "service-b":
+    case "Leap2":
         scriptPath = "test.groovy"
         break
-    case "service-c":
+    case "Leap3":
         scriptPath = "deploy.groovy"
         break
     default:
