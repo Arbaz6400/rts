fbfc75e05e9692490cfd3a57862e28ca8a1bf720
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-20 18:15:48 +0530

    Create SonarPipeline.groovy

---
+++ vars/SonarPipeline.groovy
@@ -0,0 +1,28 @@
+def call() {
+    pipeline {
+        agent any
+
+        environment {
+            SONAR_TOKEN   = credentials('sonar-token-id')
+            SONARQUBE_URL = "http://localhost:9000"
+            PROJECT_KEY   = "your-project-key"
+        }
+
+        stages {
+            stage('SonarQube Scan') {
+                steps {
+                    sh """
+                        sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} \
+                                       -Dsonar.host.url=${SONARQUBE_URL} \
+                                       -Dsonar.login=${SONAR_TOKEN}
+                    """
+                }
+            }
+            stage('Quality Gate') {
+                steps {
+                    QualityGate(PROJECT_KEY, SONAR_TOKEN, SONARQUBE_URL)
+                }
+            }
+        }
+    }
+}
