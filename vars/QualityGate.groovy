44f7773707ab91cbcc304aa5303ab7d9aa4235ec
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-20 18:22:46 +0530

    Delete vars/QualityGate.groovy

--- vars/QualityGate.groovy
+++
@@ -1,6 +0,0 @@
-import com.mycompany.quality.QualityGate
-
-def call(String projectKey, String sonarToken, String sonarUrl) {
-    def gate = new QualityGate(this)
-    gate.check(projectKey, sonarToken, sonarUrl)
-}
