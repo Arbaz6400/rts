81c9b63c48ebee9eafdc118fb2d3523b9c96734b
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-20 18:10:43 +0530

    Update QualityGate.groovy

--- vars/QualityGate.groovy
+++ vars/QualityGate.groovy
@@ -1,22 +1,6 @@
-// vars/QualityGate.groovy
-import groovy.json.JsonSlurper
+import com.mycompany.quality.QualityGate
 
-class QualityGate implements Serializable {
-
-    static void check(String projectKey, String sonarToken, String SONARQUBE_URL) {
-        // Run shell command in Jenkins
-        def status = sh(
-            script: """
-                curl -s -u ${sonarToken}: "${SONARQUBE_URL}/api/qualitygates/project_status?projectKey=${projectKey}" \
-                | jq -r '.projectStatus.status'
-            """,
-            returnStdout: true
-        ).trim()
-
-        echo "SonarQube Quality Gate Status: ${status}"
-
-        if (status != "OK") {
-            error("Quality Gate failed: ${status}")
-        }
-    }
+def call(String projectKey, String sonarToken, String sonarUrl) {
+    def gate = new QualityGate(this)
+    gate.check(projectKey, sonarToken, sonarUrl)
 }
