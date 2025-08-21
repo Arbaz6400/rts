09cb839869882e0c6abb341ba4940e018907cfe9
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-21 13:25:41 +0530

    Update Sonar.groovy

--- vars/Sonar.groovy
+++ vars/Sonar.groovy
@@ -2,32 +2,31 @@ import com.mycompany.quality.QualityGate
 
 def call(String repoName) {
     pipeline {
-        agent any
+    agent any
 
-        environment {
-            SONAR_TOKEN   = credentials('sonar-token-id')
-            SONARQUBE_URL = "http://localhost:9000"
-        }
+    environment {
+        SONARQUBE_URL = "http://localhost:9000"
+        PROJECT_KEY   = "your-project-key"
+    }
 
-        stages {
-            stage('SonarQube Scan') {
-                steps {
+    stages {
+        stage('SonarQube Scan') {
+            steps {
+                withCredentials([string(credentialsId: 'sonar-token-id', variable: 'SONAR_AUTH')]) {
                     sh """
-                        sonar-scanner \
-                          -Dsonar.projectKey=${repoName} \
-                          -Dsonar.host.url=${SONARQUBE_URL} \
-                          -Dsonar.login=${SONAR_TOKEN}
+                        sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} \
+                                      -Dsonar.host.url=${SONARQUBE_URL} \
+                                      -Dsonar.login=${SONAR_AUTH}
                     """
                 }
             }
+        }
 
-            stage('Quality Gate') {
-                steps {
-                    script {
-                        def gate = new QualityGate(this)
-                        // only repoName and token since branch is not supported
-                        gate.check(SONAR_TOKEN, repoName, "")
-                    }
+        stage('Quality Gate') {
+            steps {
+                script {
+                    def gate = new com.mycompany.quality.QualityGate(this)
+                    gate.check("sonar-token-id", PROJECT_KEY, SONARQUBE_URL)
                 }
             }
         }
