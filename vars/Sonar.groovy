8001d88b2aaec8c3c42e931a5c1ac9dfcb207cbc
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-21 13:27:17 +0530

    Update Sonar.groovy

--- vars/Sonar.groovy
+++ vars/Sonar.groovy
@@ -1,32 +1,33 @@
-import com.mycompany.quality.QualityGate
-
-def call(String repoName) {
+def call(Map config = [:]) {
     pipeline {
-    agent any
+        agent any
 
-    environment {
-        SONARQUBE_URL = "http://localhost:9000"
-        PROJECT_KEY   = "your-project-key"
-    }
-
-    stages {
-        stage('SonarQube Scan') {
-            steps {
-                withCredentials([string(credentialsId: 'sonar-token-id', variable: 'SONAR_AUTH')]) {
-                    sh """
-                        sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} \
-                                      -Dsonar.host.url=${SONARQUBE_URL} \
-                                      -Dsonar.login=${SONAR_AUTH}
-                    """
-                }
-            }
+        environment {
+            SONARQUBE_URL = config.sonarUrl ?: "http://localhost:9000"
+            PROJECT_KEY   = config.projectKey ?: "default-project"
+            SONAR_CRED_ID = config.credentialId ?: "sonar-token-id"
         }
 
-        stage('Quality Gate') {
-            steps {
-                script {
-                    def gate = new com.mycompany.quality.QualityGate(this)
-                    gate.check("sonar-token-id", PROJECT_KEY, SONARQUBE_URL)
+        stages {
+            stage('SonarQube Scan') {
+                steps {
+                    withCredentials([string(credentialsId: SONAR_CRED_ID, variable: 'SONAR_AUTH')]) {
+                        sh """
+                            sonar-scanner \
+                              -Dsonar.projectKey=${PROJECT_KEY} \
+                              -Dsonar.host.url=${SONARQUBE_URL} \
+                              -Dsonar.login=${SONAR_AUTH}
+                        """
+                    }
+                }
+            }
+
+            stage('Quality Gate') {
+                steps {
+                    script {
+                        def gate = new com.mycompany.quality.QualityGate(this)
+                        gate.check(SONAR_CRED_ID, PROJECT_KEY, SONARQUBE_URL)
+                    }
                 }
             }
         }
