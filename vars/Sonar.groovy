456400958611f0413aed02bdfa8215270d520ea7
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-21 13:40:42 +0530

    Update Sonar.groovy

--- vars/Sonar.groovy
+++ vars/Sonar.groovy
@@ -5,23 +5,33 @@ def call(Map config = [:]) {
         agent any
 
         environment {
-            SONARQUBE_URL = config.sonarUrl ?: "http://localhost:9000"
-            REPO_NAME     = config.repoName ?: env.JOB_NAME
-            BRANCH_NAME   = config.branchName ?: "main"
+            SONARQUBE_URL = "http://localhost:9000"  // keep static here
         }
 
         stages {
             stage('SonarQube Scan') {
                 steps {
-                    withCredentials([string(credentialsId: config.credentialId ?: 'sonar-token-id', variable: 'SONAR_TOKEN')]) {
-                        sh """
-                            sonar-scanner \
-                              -Dsonar.projectKey=${REPO_NAME} \
-                              -Dsonar.projectName=${REPO_NAME} \
-                              -Dsonar.sources=. \
-                              -Dsonar.host.url=${SONARQUBE_URL} \
-                              -Dsonar.login=${SONAR_TOKEN}
-                        """
+                    script {
+                        // compute dynamic values here
+                        def repoName   = config.repoName   ?: env.JOB_NAME
+                        def branchName = config.branchName ?: "main"
+                        def sonarUrl   = config.sonarUrl   ?: env.SONARQUBE_URL
+                        def credId     = config.credentialId ?: 'sonar-token-id'
+
+                        withCredentials([string(credentialsId: credId, variable: 'SONAR_TOKEN')]) {
+                            sh """
+                                sonar-scanner \
+                                  -Dsonar.projectKey=${repoName} \
+                                  -Dsonar.projectName=${repoName} \
+                                  -Dsonar.sources=. \
+                                  -Dsonar.host.url=${sonarUrl} \
+                                  -Dsonar.login=${SONAR_TOKEN}
+                            """
+                        }
+
+                        // Save to env for next stage
+                        env.REPO_NAME   = repoName
+                        env.BRANCH_NAME = branchName
                     }
                 }
             }
@@ -31,7 +41,7 @@ def call(Map config = [:]) {
                     script {
                         withCredentials([string(credentialsId: config.credentialId ?: 'sonar-token-id', variable: 'SONAR_TOKEN')]) {
                             def gate = new QualityGate(this)
-                            gate.check(SONAR_TOKEN, REPO_NAME, BRANCH_NAME)
+                            gate.check(SONAR_TOKEN, env.REPO_NAME, env.BRANCH_NAME)
                         }
                     }
                 }
