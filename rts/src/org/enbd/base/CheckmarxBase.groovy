dbe4401761f1d1ab1c1577d9adf73c443f5052a8
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-25 21:46:41 +0530

    Update CheckmarxBase.groovy

--- rts/src/org/enbd/base/CheckmarxBase.groovy
+++ rts/src/org/enbd/base/CheckmarxBase.groovy
@@ -1,16 +1,11 @@
 package org.enbd.base
 
-/**
- * Base class for Checkmarx scanning functionality.
- */
 class CheckmarxBase extends PipelineBase {
-
     CheckmarxBase(steps) {
         super(steps)
     }
 
     void runCheckmarxScan(String projectName) {
         log("Running Checkmarx scan for project: ${projectName}")
-        // Placeholder: integrate with Checkmarx Jenkins plugin or API
     }
 }
