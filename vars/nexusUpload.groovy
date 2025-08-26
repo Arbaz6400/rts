77980e2d5041905b127b19bf3315e61238691631
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-26 18:24:40 +0530

    Update nexusUpload.groovy

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -1,4 +1,6 @@
+// vars/nexusUpload.groovy
 def call(String version, String repo) {
+    // Create instance of your Nexus helper
     def nexus = new org.enbd.common.NexusRest(this)
     nexus.uploadReleaseProdNexus(version, repo)
 }
