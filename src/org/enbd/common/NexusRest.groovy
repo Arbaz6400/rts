94e4cbadffa10fb74f1ac3ef3f54595455dcb775
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-08-26 12:27:48 +0530

    Update NexusRest.groovy

--- src/org/enbd/common/NexusRest.groovy
+++ src/org/enbd/common/NexusRest.groovy
@@ -2,63 +2,85 @@ package org.enbd.common
 
 import org.enbd.base.PipelineBase
 
-/**
- * This class provides methods for interacting with a Nexus repository.
- */
 class NexusRest extends PipelineBase {
 
     NexusRest(def steps) {
         super(steps)
     }
 
-    def getJar(String nexusUrl, String credentialId, String jarName) {
-        steps.echo("getJar ${nexusUrl}")
-        return steps.httpRequest(
-            authentication: credentialId,
-            url: nexusUrl,
-            consoleLogResponseBody: false,
-            outputFile: jarName,
-            validResponseCodes: '200',
-            ignoreSslErrors: true,
-        )
-    }
+    def uploadReleaseProdNexus(String pom_location, String repository, Boolean shadowJar_plugin) {
+        def pom = this.steps.readMavenPom(file: pom_location)
+        def jar_location = shadowJar_plugin ?
+            "build/libs/${pom.artifactId}-${pom.version}-all.jar" :
+            "build/libs/${pom.artifactId}-${pom.version}.jar"
 
-    def getRepositories(String nexusUrl, String username, String password) {
-        return """#!/bin/bash -x
-        curl -u ${username}:${password} -X GET ${nexusUrl}/service/rest/v1/repositories
-        """
-    }
-
-    def getRepositoriesList(String nexusUrl, String username, String password) {
-        def response = this.getRepositories(nexusUrl, username, password)
-        return this.steps.readJSON(text: response)
-    }
-
-    def uploadReleaseProdNexus(String pomLocation, String repository, Boolean shadowJarPlugin) {
-        def pom = this.steps.readMavenPom(file: pomLocation)
-        def jarLocation = shadowJarPlugin ?
-                "build/libs/${pom.artifactId}-${pom.version}-all.jar" :
-                "build/libs/${pom.artifactId}-${pom.version}.jar"
-
-        this.steps.echo("Uploading Jar ${jarLocation} to ${repository}")
+        this.steps.echo("Uploading Jar ${jar_location} to ${repository}")
         this.steps.nexusPublisher(
             nexusInstanceId: 'nexus-server',
             nexusRepositoryId: repository,
-            packages: [
-                [
-                    $class: 'MavenPackage',
-                    mavenAssetList: [
-                        [classifier: "", extension: 'jar', filePath: jarLocation],
-                        [classifier: "", extension: 'pom', filePath: pomLocation]
-                    ],
-                    mavenCoordinate: [
-                        artifactId: pom.artifactId,
-                        groupId: pom.groupId,
-                        packaging: 'jar',
-                        version: pom.version,
-                    ]
+            packages: [[
+                $class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: "", extension: 'jar', filePath: jar_location],
+                    [classifier: "", extension: 'pom', filePath: pom_location]
+                ],
+                mavenCoordinate: [
+                    artifactId: pom.artifactId,
+                    groupId: pom.groupId,
+                    packaging: 'jar',
+                    version: pom.version
                 ]
-            ]
+            ]]
         )
+    }
+
+    def uploadEngNexusArtifact(String pom_location, String repository,
+                               Boolean shadowJar_plugin, String userpass, boolean verbose) {
+        def pom = this.steps.readMavenPom(file: pom_location)
+        def jar_location = shadowJar_plugin ?
+            "build/libs/${pom.artifactId}-${pom.version}-all.jar" :
+            "build/libs/${pom.artifactId}-${pom.version}.jar"
+
+        def group_path = pom.groupId.split("\\.").join("/")
+        def nexus_reg = "https://enqnexus.enbduat.com/repository/${repository}"
+
+        if (group_path.equals("")) {
+            this.steps.error("GroupId not specified")
+        }
+
+        ['pom': pom_location, 'jar': jar_location].each { artifact, location ->
+            this.api_call(
+                verbose: verbose,
+                httpMode: 'PUT',
+                ignoreSslErrors: true,
+                authentication: userpass,
+                uploadFile: location,
+                url: "${nexus_reg}/${group_path}/${pom.artifactId}/${pom.version}/${pom.artifactId}-${pom.version}.${artifact}"
+            )
+        }
+    }
+
+    private def api_call(Map args) {
+        def httpMode = args.get('httpMode', 'GET')
+        def ignoreSslErrors = args.get('ignoreSslErrors', false) ? "-k" : ""
+        def verbose = args.get('verbose', false) ? "-v" : ""
+        def url = args['url']
+        def authentication = args.get('authentication', '')
+        def uploadFile = args.get('uploadFile', '')
+
+        def uploadFileStr = uploadFile ? "--upload-file ${uploadFile}" : ""
+
+        def curl_command = "curl -o /dev/null -w '%{http_code}' ${verbose} ${ignoreSslErrors} -X ${httpMode} ${uploadFileStr} ${url}"
+
+        def curl_result = this.steps.sh(
+            script: "${curl_command} -u '${authentication}'",
+            returnStdout: true
+        ).trim() as Integer
+
+        if (curl_result < 200 || curl_result >= 400) {
+            this.steps.error("Upload Failed with code: ${curl_result}")
+        }
+
+        return curl_result
     }
 }
