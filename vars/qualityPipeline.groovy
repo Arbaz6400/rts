3e9b650014990d14d0701dd8e0b69ca605cdef6f
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-01-04 11:19:48 +0530

    Update qualityPipeline.groovy

--- vars/qualityPipeline.groovy
+++ vars/qualityPipeline.groovy
@@ -1,1 +1,31 @@
 
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Load Secrets') {
+                steps {
+                    script {
+                        def secretsFile = "${env.WORKSPACE}/secrets.yaml"
+
+                        def secrets = org.enbd.rts.SecretLoader.load(
+                            this,
+                            secretsFile
+                        )
+
+                        if (secrets.isEmpty()) {
+                            echo "No secrets found for this job"
+                        } else {
+                            secrets.each { k, v ->
+                                echo "Secret loaded for argument: ${k}"
+                            }
+                        }
+
+                        // You will later pass `secrets` to Flink submit
+                    }
+                }
+            }
+        }
+    }
+}
