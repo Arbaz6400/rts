8b7b316fde52e4439af16703d356e5b3bb900944
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-02-08 23:07:12 +0530

    Create flinkPipeline.groovy

---
+++ vars/flinkPipeline.groovy
@@ -0,0 +1,18 @@
+def call() {
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('Generate Jobs') {
+                steps {
+                    jobDsl(
+                        targets: 'dsl/clusters.groovy',
+                        removedJobAction: 'DELETE',
+                        removedViewAction: 'DELETE'
+                    )
+                }
+            }
+        }
+    }
+}
