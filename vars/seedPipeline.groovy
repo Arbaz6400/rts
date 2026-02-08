c951aaf4b0dc94351758eb661feccef66ae6acf3
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-02-08 23:24:09 +0530

    Fix formatting in seedPipeline.groovy

--- vars/seedPipeline.groovy
+++ vars/seedPipeline.groovy
@@ -1,15 +1,12 @@
 def call() {
-
     pipeline {
         agent any
-
         stages {
             stage('Generate Jobs') {
                 steps {
                     jobDsl(
                         targets: 'dsl/clusters.groovy',
-                        removedJobAction: 'DELETE',
-                        removedViewAction: 'DELETE'
+                        removedJobAction: 'DELETE'
                     )
                 }
             }
