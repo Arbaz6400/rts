6d07229b9933d5bcded2127487538ca2b977b40c
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-02-09 00:01:41 +0530

    Update seedPipeline.groovy

--- vars/seedPipeline.groovy
+++ vars/seedPipeline.groovy
@@ -1,15 +1,59 @@
 def call() {
     pipeline {
         agent any
+
         stages {
-            stage('Generate Jobs') {
+            stage('Generate Cluster Jobs') {
                 steps {
-                    jobDsl(
-                        targets: 'dsl/clusters.groovy',
-                        removedJobAction: 'DELETE'
-                    )
+                    script {
+                        generateClusterJobs()
+                    }
                 }
             }
         }
     }
+}
+
+def generateClusterJobs() {
+    jobDsl(
+        scriptText: buildDsl(),
+        sandbox: true
+    )
+}
+
+def buildDsl() {
+    return """
+        import groovy.io.FileType
+
+        def clustersDir = new File("\${new File('.').absolutePath}/clusters")
+
+        if (!clustersDir.exists()) {
+            throw new RuntimeException("clusters directory not found in workspace")
+        }
+
+        folder('flink-clusters')
+
+        clustersDir.eachFile(FileType.DIRECTORIES) { dir ->
+            def clusterName = dir.name
+
+            pipelineJob("flink-clusters/\${clusterName}") {
+                description("Auto-generated job for Flink cluster: \${clusterName}")
+
+                definition {
+                    cpsScm {
+                        scm {
+                            git {
+                                remote {
+                                    url('https://github.com/Leap-stream/Leap2.git')
+                                    credentials('github-pat')
+                                }
+                                branch('develop')
+                            }
+                        }
+                        scriptPath('Jenkinsfile')
+                    }
+                }
+            }
+        }
+    """
 }
