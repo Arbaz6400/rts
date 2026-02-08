3f0765ca0982bb760c7b47d471e9b3cd9e9c21d9
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-02-09 00:19:26 +0530

    Refactor seedPipeline to use jobDsl directly
    Removed the generateClusterJobs function and its invocation. Updated the pipeline to use jobDsl directly for generating cluster jobs.

--- vars/seedPipeline.groovy
+++ vars/seedPipeline.groovy
@@ -1,61 +1,20 @@
 def call() {
+
     pipeline {
         agent any
 
         stages {
             stage('Generate Cluster Jobs') {
                 steps {
-                    script {
-                        generateClusterJobs()
-                    }
+                    jobDsl(
+                        targets: 'dsl/clusters.groovy',
+                        additionalParameters: [
+                            WORKSPACE_PATH: env.WORKSPACE
+                        ],
+                        sandbox: false
+                    )
                 }
             }
         }
     }
-}
-
-def generateClusterJobs() {
-    jobDsl(
-  targets: 'dsl/**/*.groovy',
-  removedJobAction: 'DELETE',
-  removedViewAction: 'DELETE'
-)
-
-}
-
-def buildDsl() {
-    return """
-        import groovy.io.FileType
-
-        def clustersDir = new File("\${new File('.').absolutePath}/clusters")
-
-        if (!clustersDir.exists()) {
-            throw new RuntimeException("clusters directory not found in workspace")
-        }
-
-        folder('flink-clusters')
-
-        clustersDir.eachFile(FileType.DIRECTORIES) { dir ->
-            def clusterName = dir.name
-
-            pipelineJob("flink-clusters/\${clusterName}") {
-                description("Auto-generated job for Flink cluster: \${clusterName}")
-
-                definition {
-                    cpsScm {
-                        scm {
-                            git {
-                                remote {
-                                    url('https://github.com/Leap-stream/Leap2.git')
-                                    credentials('github-pat')
-                                }
-                                branch('develop')
-                            }
-                        }
-                        scriptPath('Jenkinsfile')
-                    }
-                }
-            }
-        }
-    """
 }
