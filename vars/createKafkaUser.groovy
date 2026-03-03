b01a72debc07991ac8547c4eab73f387bbf6a5ae
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-03-03 21:51:28 +0530

    Change generated password to static 'Admin123'
    Replaced dynamic password generation with a static password.

--- vars/createKafkaUser.groovy
+++ vars/createKafkaUser.groovy
@@ -12,10 +12,7 @@ def call() {
 stage('Generate Password') {
     steps {
         script {
-            env.GENERATED_PASSWORD = bat(
-                script: '''python -c "import secrets, base64; print(base64.b64encode(secrets.token_bytes(16)).decode())"''',
-                returnStdout: true
-            ).trim()
+            env.GENERATED_PASSWORD = 'Admin123'
         }
     }
 }
