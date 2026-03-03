b25d9e3b6cb562c346988d92d4325b0397bde8c3
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2026-03-03 19:32:05 +0530

    Refactor user creation logic in create_user.py

--- src/com/org/config/create_user.py
+++ src/com/org/config/create_user.py
@@ -10,28 +10,23 @@ PASSWORD = os.environ["PASSWORD"]
 
 conf = {
     "bootstrap.servers": BOOTSTRAP,
-    "security.protocol": "SASL_PLAINTEXT",  # change to SASL_SSL in real env
+    "security.protocol": "SASL_PLAINTEXT",
     "sasl.mechanism": "SCRAM-SHA-512",
     "sasl.username": ADMIN_USER,
     "sasl.password": ADMIN_PASS
 }
 
-try:
-    admin = AdminClient(conf)
+admin = AdminClient(conf)
 
-    scram = UserScramCredentialUpsertion(
-        NEW_USER,
-        ScramMechanism.SCRAM_SHA_512,
-        PASSWORD,
-        4096
-    )
+scram = UserScramCredentialUpsertion(
+    NEW_USER,
+    ScramMechanism.SCRAM_SHA_512,
+    PASSWORD,
+    4096
+)
 
-    futures = admin.alter_user_scram_credentials([scram])
+futures = admin.alter_user_scram_credentials([scram])
 
-    for u, f in futures.items():
-        f.result()
-        print(f"User {u} created successfully.")
-
-except Exception as e:
-    print("ERROR:", e)
-    sys.exit(1)
+for u, f in futures.items():
+    f.result()
+    print(f"User {u} created successfully.")
